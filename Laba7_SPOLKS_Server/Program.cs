4dcd79382c22a80bee2693bc442452b76023f47e
Fixer1008 <madridist1008@gmail.com>
2014-12-12 23:39:04 +0300

    Initial commit

---
+++ Laba7_SPOLKS_Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba7_SPOLKS_Server
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
